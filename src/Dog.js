6bbe518108b4b4841d9a24322880fb447ac6b66a
Monicacholico <monicachol@hotmail>
2020-12-21 18:16:38 -0600

    revisiting on conventions for creat react app

---
+++ src/Dog.js
@@ -0,0 +1,15 @@
+
+import React, {Component} from 'react';
+import './Dog.css';
+
+class Dog extends Component {
+    render() {
+        return(
+            <div className="Dog">
+                <h2>Dog!</h2>
+            </div>
+        )
+    }
+}
+
+export default Dog;
---
+++ src/Exercise_Fruits/Fruits.js
@@ -0,0 +1,8 @@
+
+    let arr;
+    function Fruits() {
+        return arr = ['apple', 'peach', 'orange','melon', 'strawberry', 'apple', 'kiwi', 'cherry',
+        'avocado', 'grapes', 'papaya', 'fig', 'watermelon', 'grapefruit',  'rasberry', 'blueberry'
+        ]
+    }
+
