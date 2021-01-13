930dc69a5468613c5809d05fcd751f918aad7f13
Monicacholico <monicachol@hotmail.com>
2021-01-12 18:39:20 -0800

    saving

--- src/Ball.js
+++ src/Ball.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 
+import './Ball.css';
+
 class Ball extends Component {
     render() {
-        // let arrNumbers = Array.from({length: 40}, map(el => el))
-        let arrNumbers = [];
-        let randNumber = Math.floor(Math.random * [...arrNumbers.length]) + 1;
-        console.log(randNumber)
+        let arrNumbers = Array.from({length: 4}, () => Math.floor(Math.random() * 400));
+        let ballNumbers = arrNumbers.join(' ');
+        let eachBall = ballNumbers.map(ballNumber => <h1 className="Ball">{ballNumber}</h1>)
+        let randNumber = Math.floor(Math.random * 40) + 1;
+        console.log(randNumber);
         return (
             <h2>
-                {randNumber}
+                {/* {ballNumbers.map(ballNumber => <div className="Ball">{ballNumber}</div>)} */}
+                {eachBall}
             </h2>
         )
     }
