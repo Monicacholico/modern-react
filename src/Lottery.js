7b03adb2eb70631e34c018a8e4fdfecc20f4fc0a
Monicacholico <monicachol@hotmail>
2021-01-14 19:32:41 -0600

    redesign of components

--- src/Lottery.js
+++ src/Lottery.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
 import Ball from './Ball';
 
+import './Ball.css';
+
 class Lottery extends Component {
+    static defaulProps = {
+        title: 'This is the Title',
+        numOfBalls : 0,
+        maxNum : 0
+    }
+    state = {
+        nums : Array.from({length: 4}, () => Math.floor(Math.random() * 400))
+    }
     render() {
+        console.log(this.state);
         return (
             <div className="App-wrapper">
                 <div className="App-ball">
-                <h1>I am a ball</h1>
-                <Ball/>
+                <h1>Lotto 4</h1>
+                <div className="Ball-wrapper">
+                <Ball className='Ball'
+                num={23}
+                />
+                <Ball className="Ball"
+                num={17}
+                />
+                </div>
                 </div>
             </div>
         )
