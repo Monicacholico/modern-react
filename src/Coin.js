f5b500b38911c0c2d0a839b5729c6d334d36796b
Monicacholico <monicachol@hotmail>
2021-01-20 21:47:53 -0600

    added components coin and flipManager" ;

--- src/Coin.js
+++ src/Coin.js
@@ -1,23 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
+import './Coin.css';
 
 class Coin extends Component {
- state = {
-     face: 'cara'
- }
- flip = curState => {
-    return {face: curState.face === 'cara' ? 'cruz' : 'cara'};
-}
+    static defaultProps = {
+        face: 'cara'
+    }
     render() {
         return (
-            <section className="App-wrapper">
-                <h1>Let's Flip a Coin</h1>
-                <div className="Ball">
-                    {this.state.face}
+            <section className="Coin-wrapper">
+                <div className="Coin">
+                    {this.props.face}
                 </div>
-                <button onClick={this.flip}>Flip it Here!</button>
             </section>
-        )
+            )
     }
 }
 
